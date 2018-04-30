2da8aec30eba86823d2bfc74073b91c6939422a4
Bruno Charleaux <bruno-charleaux@hotmail.com>
2018-04-30 15:42:56 -0300

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { userhome } from '../userhome/userhome';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,9 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  onuser(){
+    this.navCtrl.push(userhome);
   }
 
 }
---
+++ src/pages/userhome/userhome.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-userhome',
+  templateUrl: 'userhome.html'
+})
+export class userhome {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
